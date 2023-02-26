2e4bd728aad7580bb8542133f80faee19a58e92d
zsui0 <zsoltplezer@gmail.com>
2023-02-26 18:46:26 +0100

    Backend -> Login, Register, Server-Side Session

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,34 +1,75 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_mysqldb import MySQL
-
-#pip freeze > requirements.txt -> menti a packagek nevét
-#pip install -r requirements.txt -> a fájl segítségével leszedi az összes szükséges packaget
+from flask import Flask, request, jsonify, session
+from flask_bcrypt import Bcrypt
+from flask_session import Session
+from config import ApplicationConfig
+from models import db, User
 
 app = Flask(__name__)
+app.config.from_object(ApplicationConfig)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pedikurmanikur'
-mysql = MySQL(app)
+bcrypt = Bcrypt(app)
+server_session = Session(app)
+db.init_app(app)
 
-CORS(app)
+with app.app_context():
+    db.create_all()
 
+@app.route("/@me")
+def get_current_user():
+    user_id = session.get("user_id")
 
-@app.route("/api/members")
-def members():
-    return {"members": ["Member1","Member2","Member3"]}
+    if not user_id:
+        return jsonify({"error": "Unathourized",}), 401
+    
+    user = User.query.filter_by(user_id=user_id).first()
+    return jsonify({
+        "id": user.user_id,
+        "email": user.user_email
+    })
 
-@app.route('/api/insertdata')
-def insertdata():
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO users(first_name,last_name,user_email,user_pass,user_name,user_role) VALUES('Zsolt','Plezer','zsoltplezer@gmail.com','asd123','zsui','felh')")
-    mysql.connection.commit()
-    cur.close()
-    return "Done!!";
+@app.route("/register", methods=["POST"])
+def register_user():
+    user_email = request.json["email"]
+    user_pass = request.json["password"]
 
+    user_exists = User.query.filter_by(user_email=user_email).first() is not None
+    if user_exists:
+        return jsonify({"error": "User already exists",}), 409
+
+    first_name = request.json["first"]
+    last_name = request.json["last"]
+    user_name = request.json["name"]
+    user_role = request.json["role"]
+
+    hashed_password = bcrypt.generate_password_hash(user_pass)
+    new_user = User(user_email=user_email, user_pass=hashed_password,first_name=first_name,last_name=last_name,user_name=user_name,user_role=user_role)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        "id": new_user.user_id,
+        "email": new_user.user_email
+    })
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    user_email = request.json["email"]
+    user_pass = request.json["password"]
+
+    user = User.query.filter_by(user_email=user_email).first()
+    if user is None:
+        return jsonify({"error": "Unathourized",}), 401
+    
+    if not bcrypt.check_password_hash(user.user_pass, user_pass):
+        return jsonify({"error": "Unathourized",}), 401
+
+    session["user_id"] = user.user_id
+
+    return jsonify({
+        "id": user.user_id,
+        "email": user.user_email
+    })
 
 
 if __name__ == "__main__":
-    app.run(host="localhost",port="5000",debug=True)
+    app.run(debug=True)
