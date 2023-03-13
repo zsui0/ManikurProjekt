a8470f18c3872a9d7e75928ef1631b2a2e9c2a4c
zsui0 <zsoltplezer@gmail.com>
2023-03-13 22:29:01 +0100

    Booking :(

--- client/src/pages/Booking.jsx
+++ client/src/pages/Booking.jsx
@@ -18,49 +18,115 @@ const localizer = momentLocalizer(moment) // or globalizeLocalizer
 const dnd = withDragAndDrop(Calendar)
 const today = new Date()
 
+  var events = [
+/*   {
+    title: 'My Event',
+    start: new Date('2023-03-12T13:45:00'),
+    end: new Date('2023-03-12T16:00:00'),
+    isDraggable: true,
+    resizable: true,
+  },
+  {
+    title:'masodik',
+    start: new Date('2023-03-13T13:45:00'),
+    end: new Date('2023-03-13T16:00:00')
+  } */
+]  
+
 const Booking = (props) => {
-  const [events, setEvents] = useState([])
+  const [eventss, setEvents] = useState([]);
+  const [isLoading, setLoading] = useState();
 
   useEffect(() => {
-    const bookings = BookingService.getEvents();
-    
-    if (bookings) {
-      setEvents(bookings);
+
+    const getAllBookings = async () => {
+      setLoading()
+      const data = await BookingService.getEvents();
+      var keys = Object.keys(data);
+      for (var i = 0; i < keys.length; i++) {
+        data[i].start = Date.parse(data[i].start.substring(0,data[i].start.length-2))
+        data[i].end = Date.parse(data[i].end.substring(0,data[i].end.length-2))
+      }
+      //const jsonData = JSON.stringify(data); 
+      if (data != null){
+
+        console.log(isLoading)
+        setLoading(false);
+        setEvents(data)
+        console.log(isLoading)
+      }
     }
-  }, []);
+    getAllBookings()
 
+  }, []); 
+console.log(events)
+//console.log(eventss)
 
-  return (
-    <div className="myCustomHeight" styles="height: 10">
-      <Calendar
-        localizer={localizer}      
-        startAccessor="start"
-        endAccessor="end"
-        events={events}
-        draggableAccessor={(event) => true}
-        defaultView={Views.WEEK}
-        step={15}
-        views={['week','day']}
-        min={
-          new Date(
-            today.getFullYear(), 
-            today.getMonth(), 
-            today.getDate(), 
-            8
-          )
-        }  
-        max={
-          new Date(
-            today.getFullYear(), 
-            today.getMonth(), 
-            today.getDate(), 
-            20
-          )
-        }
-      />    
-    </div>
-  );
+if(!isLoading){
+    return (
+      <div className="myCustomHeight" styles="height: 10">
+        <Calendar
+          localizer={localizer}      
+          startAccessor="start"
+          endAccessor="end"
+          events={events}
+          draggableAccessor={(event) => true}
+          defaultView={Views.WEEK}
+          step={15}
+          views={['week','day']}
+          min={
+            new Date(
+              today.getFullYear(), 
+              today.getMonth(), 
+              today.getDate(), 
+              8
+            )
+          }  
+          max={
+            new Date(
+              today.getFullYear(), 
+              today.getMonth(), 
+              today.getDate(), 
+              20
+            )
+          }
+        />    
+      </div>
+    );
+  } else {
+    return (
+      <div className="myCustomHeight" styles="height: 10">
+        <Calendar
+          localizer={localizer}      
+          startAccessor="start"
+          endAccessor="end"
+          events={events}
+          draggableAccessor={(event) => true}
+          defaultView={Views.WEEK}
+          step={15}
+          views={['week','day']}
+          min={
+            new Date(
+              today.getFullYear(), 
+              today.getMonth(), 
+              today.getDate(), 
+              8
+            )
+          }  
+          max={
+            new Date(
+              today.getFullYear(), 
+              today.getMonth(), 
+              today.getDate(), 
+              20
+            )
+          }
+        />    
+      </div>
+    );
+  }
 }
+
 
 
 
