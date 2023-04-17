bd36f9c8c9ab400873a1dc85de3395e1ed5d930b
Callistron <istvan1995@gmail.com>
2023-04-17 21:08:29 +0200

    profil fix

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,10 +3,12 @@ import AuthService from '../services/auth.service';
   
 const Profile = (user) => {
 
-  
+  const[userName,setUserName] = useState("");
+  const[lastName,setLastName] = useState("");
+  const[firstName,setFirstName] = useState("");
+  const[email,setEmail] = useState("");
+  const[password,setPassword] = useState("");
 
-
-    
   console.log(user);
 
   return (
@@ -21,28 +23,6 @@ const Profile = (user) => {
                     
               </>  
             )}
-    </div>
-  );
-};
-
-export default Profile;
-import React, { useState} from 'react';
-import AuthService from '../services/auth.service';
-  
-const Profile = (props) => {
-
-  const[userName,setUserName] = useState("");
-  const[lastName,setLastName] = useState("");
-  const[firstName,setFirstName] = useState("");
-  const[email,setEmail] = useState("");
-  const[password,setPassword] = useState("");
-
-
-
-
-  return (
-    <div>
-    {props.currentUser === undefined ? (<p>Jelentkezzen be!</p>) : (<div>{props.currentUser.email}</div>)}
     </div>
   );
 };
