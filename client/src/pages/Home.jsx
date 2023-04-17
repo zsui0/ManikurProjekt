23b8ba11f1ae8d8b6ced75a45ab51042ba6c3fbd
Callistron <istvan1995@gmail.com>
2023-04-17 11:59:48 +0200

    carousel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
+import Carousel from 'react-bootstrap/Carousel';
   
 const Home = (user) => {
   return (
-    <div>
+    /*<div>
       <h1>Üdvözlünk a főoldalon!</h1>
       {user.currentUser === undefined ? (
         <h1>Nem vagy bejelentkezve</h1>
@@ -12,7 +13,49 @@ const Home = (user) => {
           <h1>Bevagy jelentkezve!</h1>
         </div>
       )}
-    </div>
+    </div>*/
+
+    <Carousel>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=First slide&bg=373940"
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Second slide&bg=282c34"
+        alt="Second slide"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Third slide&bg=20232a"
+        alt="Third slide"
+      />
+
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+        </p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+
+
   );
 };
   
