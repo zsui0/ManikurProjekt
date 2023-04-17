994d65563131ff8e42ce0a8d561b90c35f197c60
GergoTorokSS <gerihun96@gmail.com>
2023-04-17 12:29:46 +0200

    carousel szépítgetés

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-  
+import '../styles/carousel.scss'
 const Home = (user) => {
   return (
     /*<div>
@@ -23,8 +23,10 @@ const Home = (user) => {
         alt="First slide"
       />
       <Carousel.Caption>
+        <div class ="carouselText">
         <h3>Hagyományos manikűr</h3>
         <p>A kéz és a körmök rendszeres törődést, ápolást igényelnek. Manikűr során a bőr a radirozás és krémezés hatására megújul. A körmök japán manikűr következtében visszanyerik csillogásukat.</p>
+        </div>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
@@ -35,8 +37,10 @@ const Home = (user) => {
       />
 
       <Carousel.Caption>
+        <div class ="carouselText">
         <h3>Second slide label</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
@@ -47,10 +51,12 @@ const Home = (user) => {
       />
 
       <Carousel.Caption>
+        <div class ="carouselText">
         <h3>Third slide label</h3>
         <p>
           Praesent commodo cursus magna, vel scelerisque nisl consectetur.
         </p>
+        </div>
       </Carousel.Caption>
     </Carousel.Item>
   </Carousel>
