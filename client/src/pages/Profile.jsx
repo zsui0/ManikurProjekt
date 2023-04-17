aa4d884924cb10a7257fa07b60d8f5c80cf61ed1
GergoTorokSS <gerihun96@gmail.com>
2023-04-17 21:52:01 +0200

    Profile css

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useState} from 'react';
 import AuthService from '../services/auth.service';
 import Button from 'react-bootstrap/Button';
 import ProfilPopup from "./ProfilPopup";
+import '../styles/Profil.scss'
   
 const Profile = (user) => {
   
@@ -24,18 +25,21 @@ const Profile = (user) => {
 
   return (
     <>
-    <div class="profilDiv">
+    <section>
       {user.currentUser === undefined ? ( <></> 
             ) : (
-              <>
-                <p>Vezetéknév: {user.currentUser.lastName}</p>                
-                <p>Keresztnév: {user.currentUser.firstName}</p>                
-                <p>E-mail cím: {user.currentUser.email}</p>                
-                <p>Felhasználónév: {user.currentUser.name}</p>
-                <button class="profilButton" onClick={()=> popupButton(user.currentUser.lastName,user.currentUser.firstName,user.currentUser.email,user.currentUser.name)}>Adatok szerkesztése</button>                    
-              </>  
+              <div class="form-box">
+                <div class="form-value">
+                  <h5>Vezetéknév: {user.currentUser.lastName}</h5>                
+                  <h5>Keresztnév: {user.currentUser.firstName}</h5>                
+                  <h5>E-mail cím: {user.currentUser.email}</h5>                
+                  <h5>Felhasználónév: {user.currentUser.name}</h5>
+                  <button class="profilButton" onClick={()=> popupButton(user.currentUser.lastName,user.currentUser.firstName,user.currentUser.email,user.currentUser.name)}>Adatok szerkesztése</button>                    
+                  <button class="profilButton" onClick={()=> popupButton(user.currentUser.lastName,user.currentUser.firstName,user.currentUser.email,user.currentUser.name)}>Jelszó szerkesztése</button>
+                </div>
+              </div>
             )}
-    </div>
+    </section>
     <ProfilPopup trigger={buttonPopup} setTrigger={setButtonPopup}  lastName={user.currentUser.lastName} firstName={user.currentUser.firstName} email={user.currentUser.email} userName={user.currentUser.name}>  
     </ProfilPopup>
     </>
