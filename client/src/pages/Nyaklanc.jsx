68b74a2dfb10afcba2597e5d4c4ffdca95a6ef2e
Callistron <istvan1995@gmail.com>
2023-04-17 15:22:24 +0200

    nyaklanc fix

--- client/src/pages/Nyaklanc.jsx
+++ client/src/pages/Nyaklanc.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import {Card} from "react-bootstrap";
 
 const componentInfo=[
-    {image: "'../icons/ekszerek/neck1.jpg'"},      
+    {image: "neck1.jpg"},
+    {image: "neck2.jpg"},
+    {image: "neck3.jpg"},
+    {image: "neck4.jpg"},
+    {image: "neck5.jpg"},
+    {image: "neck6.jpg"},
 ];
 
 const Nyaklanc=()=>{
     const renderCard=(card, index) =>{
         return(
             <Card style={{ width: '18rem'}} key={index} className="card">
-                <Card.Img variant="top" src={()=>require(card.image)}/>
+                <Card.Img variant="top" src={require("../icons/ekszerek/"+card.image)}/>
             </Card>
         );
     };
