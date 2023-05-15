4227eaf12db27ff3d23144afa02de4787ae77e46
Astil5 <ittvokrehakadam@gmail.com>
2023-05-15 16:24:45 +0200

    Drag n drop fogalasok 1

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -10,7 +10,8 @@ router.post('/', authenticateToken,  async (req, res) => {
       userId: req.user._id,
       title: req.body.title,
       start: req.body.startDate,
-      end: req.body.endDate
+      end: req.body.endDate,
+      length : req.body.length
     })
     const newBooking = await appointment.save();
     res.status(201).json(newBooking);
@@ -22,13 +23,25 @@ router.post('/', authenticateToken,  async (req, res) => {
 
 router.get('/', authenticateToken, async (req, res) => {
   try{
-    const bookings = await Booking.find().lean().select('-_id title start end')
+    const bookings = await Booking.find().lean().select('_id userId title start end length')
     res.status(200).json({result: bookings})
   } catch (error) {
     res.status(500).json({message: error.message})
   }
 })
 
+router.patch('/:id', getUser, async (req, res) => {  // patch update just the new data, put update everything
+  if(req.body.lastName != null) {
+    res.user.lastName = req.body.lastName
+  }
+  //....
+  try {
+    const updatedUser = await res.user.save()
+    res.json(updatedUser)
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  }
+})
 
 function authenticateToken(req, res, next) { // middleware
   const authHeader = req.headers['authorization']
