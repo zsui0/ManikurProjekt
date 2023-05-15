e2a386fb1aab61d8a1690b1d765c32aef7783891
GergoTorokSS <gerihun96@gmail.com>
2023-05-15 18:35:24 +0200

    Fájl feltöltését kezelő cucc

---
+++ client/src/pages/UploadHandler.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class UploadHandler extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedFile: null,
+      }
+      this.props = props;
+   
+  }
+onChangeHandler=event=>{
+  var files = event.target.files
+
+     this.setState({
+     selectedFile: files,
+  })
+  console.log(this.state.selectedFile);
+} 
+  onClickHandler = () => {
+    const data = new FormData() 
+      data.append('file', this.state.selectedFile[0])
+    axios.post("http://localhost:5000/upload", data, {
+    })
+    }
+
+  render() {
+    return (
+        <>
+        
+        <div className="inputDiv">
+            <label className="input-label">Válasszon ki egy fájlt:</label>
+            <input className="text-input" id="nOut" type="file" onChange={this.onChangeHandler}></input>
+        </div>
+        <div>
+            <button className="pButtonLeft" onClick={()=>this.props.setTrigger(false)}>Vissza</button>
+            <button className="pButtonRight" onClick={this.onClickHandler}>Mentés</button>
+        </div>
+        
+    
+        </> 
+    );
+  }
+}
+
+export default UploadHandler;
