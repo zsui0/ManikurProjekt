101aeb2befa55c5a1487c28216e0b92d62502090
GergoTorokSS <gerihun96@gmail.com>
2023-05-15 18:35:10 +0200

    File feltöltés

--- client/src/pages/PopupII.js
+++ client/src/pages/PopupII.js
@@ -1,6 +1,6 @@
 import React, { useState,useEffect,ChangeEvent } from "react";
 import '../styles/Popup.scss'
-import FileUploadSingle from "./FileUploadSingle.tsx";
+import UploadHandler from "./UploadHandler";
 import GalleryJewelryService from "../services/gallery-jewelry-service";
 
 function Popup2(props) {
@@ -28,41 +28,12 @@ function Popup2(props) {
         props.setTrigger(false);
     }
 
-    const [file, setFile] = useState([]);
-
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setFile(e.target.files[0]);
-    }
-    console.log(file.size);
-  };
-
-  const handleUploadClick = () => {
-    if (!file) {
-      return;
-    }
-
-    // 👇 Uploading the file using the fetch API to the server
-    fetch('https://httpbin.org/post', {
-      method: 'POST',
-      body: file,
-      // 👇 Set headers manually for single file upload
-      headers: {
-        'content-type': file.type,
-        'content-length': `${file.size}`, // 👈 Headers need to be a string
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
-  };
-
     return (props.trigger) ?( 
         <div className="popup">
             <div className="popup-inner" >
                 <h1>Kérem adja meg az adatokat:</h1>
                 <form>
-                <FileUploadSingle props={props}/>
+                <UploadHandler props={props}/>
                 </form>
                 {props.children}
             </div>
